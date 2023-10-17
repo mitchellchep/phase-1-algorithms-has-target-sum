ecad44d363fe0eb94990cc0234d4f1eebde33a5e
mitchellkoech <mitchikoechi3.7@gmail.com>
2023-10-17 10:27:41 +0300

    Completed assignment
    Signed-off-by: mitchellkoech <mitchikoechi3.7@gmail.com>

--- index.js
+++ index.js
@@ -1,34 +1,38 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  //  empty set to store seen numbers.
+  const seenNumbers = new Set();
+
+  // Iterate through the array.
+  for (let num of array) {
+    //  diffbwn  target and current no.
+    const difference = target - num;
+
+    // Check if the diff exists in the seen no.s
+    if (seenNumbers.has(difference)) {
+      return true; // Found target
+    }
+
+    // If not found, add current no to seen no.s
+    seenNumbers.add(num);
+  }
+
+  return false; // No pair found that adds up to target
 }
 
-/* 
-  Write the Big O time complexity of your function here
-*/
+// Big O time complexity is O(n), where n is the number of elements in the array.
 
-/* 
-  Add your pseudocode here
-*/
+// Custom tests
+console.log("Expecting: true");
+console.log("=>", hasTargetSum([3, 8, 12, 4, 11, 7], 10));
 
-/*
-  Add written explanation of your solution here
-*/
+console.log("");
 
-// You can run `node index.js` to view these console logs
-if (require.main === module) {
-  // add your own custom tests in here
-  console.log("Expecting: true");
-  console.log("=>", hasTargetSum([3, 8, 12, 4, 11, 7], 10));
+console.log("Expecting: true");
+console.log("=>", hasTargetSum([22, 19, 4, 6, 30], 25));
 
-  console.log("");
+console.log("");
 
-  console.log("Expecting: true");
-  console.log("=>", hasTargetSum([22, 19, 4, 6, 30], 25));
-
-  console.log("");
-
-  console.log("Expecting: false");
-  console.log("=>", hasTargetSum([1, 2, 5], 4));
-}
+console.log("Expecting: false");
+console.log("=>", hasTargetSum([1, 2, 5], 4));
 
 module.exports = hasTargetSum;
